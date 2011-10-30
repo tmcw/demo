d946d3405dd8ae2e64be1464a817d0b90244bc1d
Tom MacWright <tom@macwright.org>
2011-10-30 14:22:12 -0400

    mass redistricting

---
+++ massredistrict/js/site.js
@@ -0,0 +1,13 @@
+var mm = com.modestmaps, map;
+$(window).load(function() {
+  wax.tilejson('http://d.tiles.mapbox.com/v2/mapbox.world-bright,tmcw.census-redistricting-massachusetts.jsonp',
+    function(tj) {
+      map = new mm.Map('map', new wax.mm.connector(tj));
+      map.setCenterZoom(
+       new mm.Location(
+         41.9911,
+         -71.73935
+      ), 9);
+      wax.mm.interaction(map, tj);
+    });
+});
